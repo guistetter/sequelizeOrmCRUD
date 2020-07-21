c680744f42cfbd6db94779d93bed2700596d7e18
guistetter <guilherme_stetter@hotmail.com>
2020-07-21 14:14:57 -0300

    criando app

---
+++ models/index.js
@@ -0,0 +1,27 @@
+const Sequelize = require("sequelize")
+
+const sequelize = new Sequelize("cadastroOrm", "root", "123456",{
+  dialect: "mysql",
+  host: "127.0.0.1"
+})
+sequelize.import('./pessoa.js')
+
+// sequelize.sync().then(() => console.log("synced"))
+//teste conexao 
+//sequelize.authenticate().then(() => console.log("ok"))
+
+// const Pessoa = sequelize.define("Pessoa", {
+//   nome: Sequelize.STRING, 
+//   cargo: Sequelize.STRING
+// })
+
+// sequelize.sync().then(() => console.log("synced"))
+
+//sequelize.import("./pessoa.js")
+
+
+/*
+"mysql2": "^1.6.5",
+    "sequelize": "^5.8.6"
+
+*/
