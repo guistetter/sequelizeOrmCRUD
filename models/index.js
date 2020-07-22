356fa489106ea8db88b12ee8eced5f63c2f2184d
guistetter <guilherme_stetter@hotmail.com>
2020-07-22 11:42:56 -0300

    relacao pessoa - model

--- models/index.js
+++ models/index.js
@@ -18,7 +18,11 @@ fs
     console.log(model.name)
 })
 
-const pessoa = 
+Object.keys(models).forEach(modelName => {
+  if("associate" in models[modelName]){
+    models[modelName].associate(models)
+  }
+})
 
 module.exports = {
   sequelize,
--- models/pessoa.js
+++ models/pessoa.js
@@ -4,6 +4,11 @@ const PessoaModel = (sequelize, DataTypes) => {
     cargo: DataTypes.STRING,
     nascimento: DataTypes.DATE
   })
+  
+  Pessoa.associate = ({Usuario}) => {
+    Pessoa.hasOne(Usuario)
+  }
+
   return Pessoa
 }
 
